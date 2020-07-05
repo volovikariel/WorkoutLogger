6add58bd232a08b2e9417e3b3d619c892b1a8310
volovikariel <volovikariel@gmail.com>
2020-07-05 15:32:02 -0400

    Separated PG from main

--- App/index.js
+++ App/index.js
@@ -6,6 +6,7 @@ const path = require('path');
 
 // Event handlers from HTML let currTable = null;
 let currTable = 'none';
+
 function exercisesClicked() {
     queryTable('exercise');
     currTable = 'exercise';
@@ -22,6 +23,13 @@ function historyClicked() {
 } 
 
 let tableList = ['exercise', 'exercise_routine', 'workout_history'];
+
+let res;
+ipcRenderer.on('got-query', (event, args) => {
+    res = args;
+    alert(`Result: ${res}`);
+    document.getElementById('placeHolder').innerHTML += 'got query';
+});
 
 // Select and display a table dynamically
 let queryTable = function(tableName) {
@@ -42,61 +50,61 @@ let queryTable = function(tableName) {
         table.appendChild(document.createElement('thead'));
         table.querySelector('thead').appendChild(document.createElement('tr'));
         
+        // IPC MAGIC
+        ipcRenderer.send('fetch-result', `SELECT * FROM ${tableName};`);
         
-        //TODO: Send the table name and statement, get the result from Main process
-        db.query(`SELECT * FROM ${tableName}`, (err, res) => {
-            // Setting up the COLUMNS
-            for(let i = 0; i < Object.keys(res.rows[0]).length; i++) {
-                let heading = document.createElement('th');
-                heading.textContent = Object.keys(res.rows[0])[i];
-                table.querySelector('thead tr').appendChild(heading);
-            }
-            document.getElementById('wrapper').appendChild(table);
-
-            // Setting up the ROWS
-            for(let i = 0; i < res.rowCount; i++) {
-                let data = Object.values(res.rows[i]);
-                let row = document.createElement('tr');
-        
-                for(let j = 0; j < Object.keys(res.rows[0]).length; j++) {
-                    let dataCell = document.createElement('td'); 
-                    dataCell.textContent = data[j];
-                    dataCell.classList.add(`${tableName}-${Object.keys(res.rows[0])[j]}`); // Cell now has a classlist of .Object.keys(res.rows[0])[n] (which is the column name)
-                    row.appendChild(dataCell);
-                }
-                table.appendChild(row);
-            }
-
-            for(let col = 0; col < Object.keys(res.rows[0]).length; col++) {
-                let cells = document.querySelectorAll(`.${tableName}-${Object.keys(res.rows[0])[col]}`); 
-                console.log(`cell length is: ${cells.length}`);
-                for(let row = 0; row < cells.length; row++) {
-                    cells[row].addEventListener('click', function() {                   
-                        console.log(
-                            `Content: ${cells[row].innerHTML}\n` +                               // Column data
-                            `Column: ${Object.keys(res.rows[0])[col]}\n` +                       // Column selected
-                            `${Object.keys(res.rows[0])[0]}: ${Object.values(res.rows[row])[0]}` // Primary-Key
-                        );
-                        document.getElementById('placeHolder').innerHTML = 
-                            `Content: ${cells[row].innerHTML}<br>` +                             // Column data
-                            `Column: ${Object.keys(res.rows[0])[col]}<br>` +                     // Column selected
-                            `${Object.keys(res.rows[0])[0]}: ${Object.values(res.rows[row])[0]}` // Primary-Key
-                    })
-                }
-            }
+        //---------
+        for(let i = 0; i < Object.keys(res.rows[0]).length; i++) {
+            let heading = document.createElement('th');
+            heading.textContent = Object.keys(res.rows[0])[i];
+            table.querySelector('thead tr').appendChild(heading);
         }
-    )} 
+        document.getElementById('wrapper').appendChild(table);
+
+        // Setting up the ROWS
+        for(let i = 0; i < res.rowCount; i++) {
+            let data = Object.values(res.rows[i]);
+            let row = document.createElement('tr');
     
-    // After it's created and/or if it's already created, display it
-    for(let i = 0; i < tableList.length; i++) {
-        if(document.getElementById(`table-${tableList[i]}`) != null) {
-            if(tableName != tableList[i]) { document.getElementById(`table-${tableList[i]}`).classList.add('hidden'); } else {
-                document.getElementById(`table-${tableList[i]}`).classList.remove('hidden');
+            for(let j = 0; j < Object.keys(res.rows[0]).length; j++) {
+                let dataCell = document.createElement('td'); 
+                dataCell.textContent = data[j];
+                dataCell.classList.add(`${tableName}-${Object.keys(res.rows[0])[j]}`); // Cell now has a classlist of .Object.keys(res.rows[0])[n] (which is the column name)
+                row.appendChild(dataCell);
+            }
+            table.appendChild(row);
+        }
+
+        for(let col = 0; col < Object.keys(res.rows[0]).length; col++) {
+            let cells = document.querySelectorAll(`.${tableName}-${Object.keys(res.rows[0])[col]}`); 
+            console.log(`cell length is: ${cells.length}`);
+            for(let row = 0; row < cells.length; row++) {
+                cells[row].addEventListener('click', function() {                   
+                    console.log(
+                        `Content: ${cells[row].innerHTML}\n` +                               // Column data
+                        `Column: ${Object.keys(res.rows[0])[col]}\n` +                       // Column selected
+                        `${Object.keys(res.rows[0])[0]}: ${Object.values(res.rows[row])[0]}` // Primary-Key
+                    );
+                    document.getElementById('placeHolder').innerHTML = 
+                        `Content: ${cells[row].innerHTML}<br>` +                             // Column data
+                        `Column: ${Object.keys(res.rows[0])[col]}<br>` +                     // Column selected
+                        `${Object.keys(res.rows[0])[0]}: ${Object.values(res.rows[row])[0]}` // Primary-Key
+                })
             }
         }
     }
 
+// After it's created and/or if it's already created, display it
+for(let i = 0; i < tableList.length; i++) {
+    if(document.getElementById(`table-${tableList[i]}`) != null) {
+        if(tableName != tableList[i]) { document.getElementById(`table-${tableList[i]}`).classList.add('hidden'); } else {
+            document.getElementById(`table-${tableList[i]}`).classList.remove('hidden');
+        }
+    }
 }
+
+}
+
 
 // Event Handlers
 function addButtonClicked() {
